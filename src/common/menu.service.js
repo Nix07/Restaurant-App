9ed9c5cd6509e1bb28c6cd252edd4630819ccac1
Nix07 <nikhil07prakash@gmail.com>
2017-05-25 10:48:58 +0530

    Initial Category View

--- src/common/menu.service.js
+++ src/common/menu.service.js
@@ -15,6 +15,18 @@ function MenuService($http, ApiPath) {
     });
   };
 
+
+  service.getMenuItems = function (category) {
+    var config = {};
+    if (category) {
+      config.params = {'category': category};
+    }
+
+    return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
+      return response.data;
+    });
+  };
+
 }
 
 
